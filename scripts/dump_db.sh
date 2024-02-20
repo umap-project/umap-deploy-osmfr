1fb2939c46b38d5eea7dc595d21c329c82728d78
Yohan Boniface <yohanboniface@free.fr>
2024-02-20 18:15:48 +0100

    feat: keep more than one pg backup

--- scripts/dump_db.sh
+++ scripts/dump_db.sh
@@ -2,8 +2,10 @@
 set -e
 
 DST=/srv/umap/pgdump
+MONTHDAY=$(date +"%d")
 
 sudo -u umap mkdir -p $DST
 sudo -u postgres pg_dump umap | bzip2 > /tmp/umap.sql.bz2
 sudo chown umap:users /tmp/umap.sql.bz2
-sudo -u umap mv /tmp/umap.sql.bz2 $DST/umap.sql.bz2
+sudo -u umap mv /tmp/umap.sql.bz2 $DST/umap.$MONTHDAY.sql.bz2
+sudo -u umap ln -sf $DST/umap.$MONTHDAY.sql.bz2 $DST/umap.latest.sql.bz2
